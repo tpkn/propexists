866855c9b33b4dfe15cfef9ecc04663c9c4fbe31
tpkn <tpkn@users.noreply.github.com>
2019-12-12 22:26:02 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,51 @@
-# propexists
+# propexists  [![npm Package](https://img.shields.io/npm/v/propexists.svg)](https://www.npmjs.org/package/propexists)
 Quick check if a nested property exists
+
+
+
+## API
+```javascript
+propexists(obj, property)
+```
+
+
+### obj
+**Type**: _Object_   
+Target object
+
+
+### property
+**Type**: _String_ | _Array_   
+Property path string `'a.b.c.d'` or an array `['a', 'b', 'c', 'd']`
+
+
+### @return
+**Type**: _Boolean_   
+
+
+
+## Usage
+```javascript
+const propexists = require('propexists');
+
+var obj = {
+   a: 1,
+   b: {
+      f: null
+      d: {
+         e: 0
+      },
+   }
+};
+
+propexists(obj, 'x')     // => false
+propexists(obj, 'a')     // => true
+propexists(obj, 'b.f')   // => false
+propexists(obj, 'b.d.e') // => true
+
+// Or using array of properties
+propexists(obj, [ 'b', 'd', 'e' ]) // => true
+```
+
+
+
---
+++ propexists.js
@@ -0,0 +1,40 @@
+/*!
+ * propexists, http://tpkn.me/
+ */
+function propexists(obj, property){
+   if(typeof obj !== 'object' || obj === null){
+      throw new TypeError('no target object!');
+   }
+   if(typeof property !== 'string' && !Array.isArray(property)){
+      throw new TypeError('second argument should be a String or an Array!');
+   }
+
+   var level = obj;
+   var props = property;
+   var result = true;
+
+   if(typeof property === 'string'){
+      props = property.split('.');
+   }
+
+   for(var i = 0, len = props.length; i < len; i++){
+      level = level[props[i]];
+
+      if(level === null || typeof level === 'undefined'){
+         result = false;
+         break;
+      }
+   }
+
+   return result;
+}
+
+if(typeof module !== 'undefined' && typeof module.exports !== 'undefined'){
+   module.exports = propexists;
+}else{
+   if(typeof define === 'function' && define.amd){
+      define([], function(){
+         return propexists;
+      });
+   }
+}
